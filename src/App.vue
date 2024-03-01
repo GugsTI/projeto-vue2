6c5817e7cd4de2473e124b0a60197df6b690b1a5
GugsTI <gustavobuenodaccb@gmail.com>
2024-03-01 11:36:21 -0300

    atualização surge

--- src/App.vue
+++ src/App.vue
@@ -26,61 +26,93 @@
 
 <template>
   <div class="info">
-    <h2>Informações</h2>
-    <p>Nome: {{ nome }} </p>
-    <p>Idade:{{ idade }} anos</p>
+    <h2 class="devInfo">INFORMAÇÕES</h2>
+    <p class="edit">Nome: {{ nome }} </p>
+    <p class="edit">Idade:{{ idade }} anos</p>
     <hr />
-    <p>
+    <p class="edit">
       Para completar 50 anos faltam: {{ 50 - idade }} anos
     </p>
-    <p>O nome tem {{ nome.length }}  caracteres</p>
-    <p>
+    <p class="edit">O nome tem {{ nome.length }}  caracteres</p>
+    <p class="edit">
       O nome invertido em maiúscula é:{{
       inverterTextoEmMaiusculo(nome) }} 
     </p>
   </div>
   <br>
   <div class="contador">
-    <h2>Contador</h2>
+    <h2 class="devCon">Contador</h2>
     <p>Valor: {{ contador }}</p>
     <button class="botao" @click="incrementar">Incrementar</button>
     <button class="botao" @click="decrementar">Decrementar</button>
     <button class="botao" @click="reiniciar">Reiniciar</button>
     <div
     >
-      <p v-if="contador > 10">Valor maior que 10</p>
-      <p v-else>Valor menor que 10</p>
+    <br>
+      <p class="Maior"   v-if="contador > 10">Valor maior que 10</p>
+      <p class="Menor" v-else>Valor menor que 10</p>
     </div>
   </div>
 </template>
 
 <style scoped>
+.edit{
+  font-family: 'Times New Roman', Times, serif;
+}
+.devInfo{
+  color:chartreuse;
+  font-family: 'Times New Roman', Times, serif;
+  
+}
+.Menor{
+  color: red;
+}
+.Maior{
+  color: cornflowerblue;
+}
+
+.devCon{
+  color: cornflowerblue;
+}
 .info{
   background-color: #000000;
   text-align: center;
   font-size: 35px;
   color: white;
   border-radius: 50px;
-  width: 280%;
+  width: 200%;
   height: 500px;
   margin-bottom: 100%;
-margin-left: -200px;}
+margin-left: 30px;
+margin-top: 25px;
+box-shadow: 10px 10px 5px 12px chartreuse;
+}
 
 .contador{
+  font-family:monospace;
   margin-top: 100%;
   background-color: #000000;
   text-align: center;
   font-size: 35px;
   color: white;
   border-radius: 50px;
-  width: 280%;
+  width: 200%;
   height: 400px;
-  margin-top: -90%;
-  margin-left: -200px;}
+  margin-top: -93.2%;
+  margin-left: 25px;
+  box-shadow: 10px 10px 5px 12px cornflowerblue;
+}
 
   .botao{
-    border-radius: 30px;
-    width: 100px;
+    background-color:cornflowerblue; 
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 30px;
 
     
 
